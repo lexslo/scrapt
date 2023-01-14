3546a92defc0be08562df9b58708bcf3a6f1bf3c
Sparkle Party <lexi.slovik@gmail.com>
2023-01-13 17:13:49 -0800

    add task goes to top of list

--- assets/script.js
+++ assets/script.js
@@ -13,12 +13,14 @@ $(document).ready(function () {
 
 // add task button
 var addTask = function(taskText) {
-    var task = $('<input type="checkbox">').addClass('task-line');
-    var taskLabel = $('<label for="to-do-2">').text(taskText);
-    $("#today-section").append(task, taskLabel);
+    var taskDiv = $('<div>');
+    var task = $('<input type="checkbox" name="to-do-3" id="to-do-3">').addClass('task-line');
+    var taskLabel = $('<label for="to-do-3">').text(taskText);
+    var newTask = taskDiv.append(task, taskLabel);
+    $("#today-tasks").prepend(newTask);
 };
 
 $("#add-task-btn").click(function() {
     console.log("add task button clicked");
-    addTask('testing add new text button');
+    addTask(' testing add new text button');
 });
