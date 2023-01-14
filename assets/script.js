eebdca0926446ac26606994fe2f6440196632225
Sparkle Party <lexi.slovik@gmail.com>
2023-01-13 17:33:14 -0800

    can add task with custom input text

--- assets/script.js
+++ assets/script.js
@@ -12,15 +12,21 @@ $(document).ready(function () {
 });
 
 // add task button
-var addTask = function(taskText) {
+var addTask = function() {
+    // new div for individual task
     var taskDiv = $('<div>');
+    // create the checkbox
     var task = $('<input type="checkbox" name="to-do-3" id="to-do-3">').addClass('task-line');
+    // grab the text from the text input next to add task btn
+    var taskText = $("#add-task-today-text").val();
+    // create label with text from input
     var taskLabel = $('<label for="to-do-3">').text(taskText);
+    // append the input and label to the div
     var newTask = taskDiv.append(task, taskLabel);
+    // prepend entire new task to the section so newest ones appear at the top
     $("#today-tasks").prepend(newTask);
 };
 
 $("#add-task-btn").click(function() {
-    console.log("add task button clicked");
-    addTask(' testing add new text button');
+    addTask();
 });
