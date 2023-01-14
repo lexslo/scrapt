1a583143148f7f1e28d11955523d091cf07d7a10
Sparkle Party <lexi.slovik@gmail.com>
2023-01-13 17:05:37 -0800

    able to add task button

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,24 @@
+// scrollbar menu functions
 $(document).ready(function () {
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
 
     $('#sidebarCollapse').on('click', function () {
-        console.log("menu button clicked");
         $('#sidebar, #content').toggleClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
+});
+
+// add task button
+var addTask = function(taskText) {
+    var task = $('<input type="checkbox">').addClass('task-line');
+    var taskLabel = $('<label for="to-do-2">').text(taskText);
+    $("#today-section").append(task, taskLabel);
+};
+
+$("#add-task-btn").click(function() {
+    console.log("add task button clicked");
+    addTask('testing add new text button');
 });
