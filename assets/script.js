eed034f6ec9352f13bae6a3dc0ff754e0a119481
Sparkle Party <lexi.slovik@gmail.com>
2023-01-16 19:22:18 -0800

    add local storage for data persistence

--- assets/script.js
+++ assets/script.js
@@ -11,48 +11,68 @@ $(document).ready(function () {
     });
 });
 
+// declare empty object for tasks in local storage
+var tasks = [];
+
 // add task button
-var addTask = function(taskDay) {
+var addTask = function(taskText, taskDay) {
     // new div for individual task
     var taskDiv = $('<div>');
     // create the checkbox
-    var task = $('<input type="checkbox" name="to-do-3" id="to-do-3">').addClass('task-line');
-    // grab the text from the text input next to add task btn
-    var taskText = $(`#add-task-${taskDay}-text`).val();
+    var task = $('<input type="checkbox" name="to-do-item" id="to-do-item">').addClass('task-line');
+    // // grab the text from the text input next to add task btn
+    // var taskText = $(`#add-task-${taskDay}-text`).val();
     // create label with text from input
-    var taskLabel = $('<label for="to-do-3">').text(taskText);
+    var taskLabel = $('<label for="to-do-item">').text(taskText);
     // append the input and label to the div
     var newTask = taskDiv.append(task, taskLabel);
     // prepend entire new task to the section so newest ones appear at the top
     $(`#${taskDay}-tasks`).prepend(newTask);
+
+    // save in tasks array
+    tasks.push({
+        text: taskText,
+        day: taskDay
+    });
 };
 
-// enable add task button once text is entered in the input field
-// $(".new-task-input").click(function() {
-//     var parentId = $(this).parent().attr('id');
-//     console.log(parentId);
-//     
+var loadTasks = function() {
+    // grab the items from local storage
+    let tasks = localStorage.getItem('tasks')
+    // if there are saved tasks
+    if (tasks) {
+        // parse into array
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+        // loop through array
+        tasks.forEach(function(task) {
+            console.log(task);
+            // add each task to appropriate place
+            addTask(task.text, task.day);
+        })
+    }
+    else {
+        return []
+    }
+};
 
-//     if ($(this).val().trim() != "") {
-//         //Enable the TextBox when TextBox has value.
-//         taskButton.removeAttr("disabled");
-//     } else {
-//         //Disable the TextBox when TextBox is empty.
-//         taskButton.attr("disabled", "disabled");
-//     }
-// });
+// use local storage to save tasks
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// keyup to track input text in input fields
 $(".new-task-input").keyup(function () {
     var parentId = $(this).parent().attr('id');
     // console.log(parentId);
-    //Reference the Button.
+    // reference the add task button
     var taskButton = $(`#add-task-${parentId}-btn`);
 
-    //Verify the TextBox value.
+    // verify there is text in the input field
     if ($(this).val().trim() != "") {
-        //Enable the TextBox when TextBox has value.
+        // enable the btn when input field has a value
         taskButton.removeAttr("disabled");
     } else {
-        //Disable the TextBox when TextBox is empty.
+        // disable the btn if the user deletes text and the input is empty again
         taskButton.attr("disabled", "disabled");
     }
 });
@@ -61,10 +81,17 @@ $(".new-task-input").keyup(function () {
 $(".add-task-btn").click(function() {
     var parentId = $(this).parent().attr('id');
     var taskButton = $(`#add-task-${parentId}-btn`);
+    // grab the text from the text input next to add task btn
+    var taskText = $(`#add-task-${parentId}-text`).val();
 
-    addTask(parentId);
+    addTask(taskText, parentId);
     // clear the input field(s)
     $(`#add-task-${parentId}-text`).val('');
     // disable add task btn once clicked
     taskButton.attr("disabled", "disabled");
+
+    saveTasks();
 });
+
+// load tasks from local storage
+loadTasks();
